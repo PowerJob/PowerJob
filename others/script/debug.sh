a2725d21cc03c4fbf794c5125535b24621c010aa
tjq <tengjiqi@gmail.com>
2020-05-18 21:56:29 +0800

    [dev] add debug script

--- others/script/debug.sh
+++ others/script/debug.sh
@@ -4,7 +4,7 @@ cd `dirname $0`/../.. || exit
 echo "================== 构建 jar =================="
 mvn clean package -DskipTests -Pdev -e -U
 echo "================== 拷贝 jar =================="
-/bin/cp -rf oh-my-scheduler-server/target/*.jar others/deploy/oms-server.jar
-ls -l others/deploy/oms-server.jar
+/bin/cp -rf oh-my-scheduler-server/target/*.jar others/oms-server.jar
+ls -l others/oms-server.jar
 echo "================== debug 模式启动 =================="
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar others/deploy/oms-server.jar
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar others/oms-server.jar
