b189cd0ba7ddcc0c478167f1a5680e0a8bd3376e
tjq <tengjiqi@gmail.com>
2020-06-24 16:49:58 +0800

    [dev] change pom to version 3.1.1

--- others/script/jenkins_auto_build.sh
+++ others/script/jenkins_auto_build.sh
@@ -29,26 +29,27 @@ docker run -d \
        -e PARAMS="--spring.profiles.active=product --spring.datasource.core.jdbc-url=jdbc:mysql://124.70.67.79:3306/powerjob-product?useUnicode=true&characterEncoding=UTF-8 --spring.data.mongodb.uri=mongodb://124.70.67.79:27017/powerjob-product" \
        -v ~/docker/powerjob-server:/root/powerjob-server -v ~/.m2:/root/.m2 \
        tjqq/powerjob-server:latest
-echo "================== powerjob-client 启动完成 =================="
-sleep 45
+sleep 60
 echo "================== 准备启动 powerjob-agent =================="
 serverIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' powerjob-server)
 serverAddress="$serverIP:7700"
 echo "使用的Server地址：$serverAddress"
 
 docker run -d \
+       --restart=always \
        --name powerjob-agent \
        -p 27777:27777 -p 5002:5005 -p 10002:10000 \
        -e JVMOPTIONS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=10000 -Dcom.sun.management.jmxremote.rmi.port=10000 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false" \
        -e PARAMS="--app powerjob-agent-test --server $serverAddress" \
        -v ~/docker/powerjob-agent:/root \
-       tjqq/powerjob-agent:$version
+       tjqq/powerjob-agent:latest
 
 docker run -d \
        --name powerjob-agent2 \
+       --restart=always \
        -p 27778:27777 -p 5003:5005 -p 10003:10000 \
        -e JVMOPTIONS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=10000 -Dcom.sun.management.jmxremote.rmi.port=10000 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false" \
        -e PARAMS="--app powerjob-agent-test --server $serverAddress" \
        -v ~/docker/powerjob-agent2:/root \
-       tjqq/powerjob-agent:$version
+       tjqq/powerjob-agent:latest
 
