987c016e24bdfca6f2125b6acb6bbdddb7581d2f
tjq <tengjiqi@gmail.com>
2020-07-19 22:39:32 +0800

    [opt] optimize custom table prefix logic

--- powerjob-server/src/main/java/com/github/kfcfans/powerjob/server/persistence/config/PowerJobPhysicalNamingStrategy.java
+++ powerjob-server/src/main/java/com/github/kfcfans/powerjob/server/persistence/config/PowerJobPhysicalNamingStrategy.java
@@ -1,14 +1,12 @@
 package com.github.kfcfans.powerjob.server.persistence.config;
 
+import com.github.kfcfans.powerjob.server.common.utils.PropertyUtils;
 import org.hibernate.boot.model.naming.Identifier;
-import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy;
 import org.springframework.util.StringUtils;
 
 import java.io.Serializable;
-import java.util.Locale;
 
 /**
  * 自定义表前缀，配置项 oms.table-prefix 不配置时，不增加表前缀。
@@ -21,24 +19,10 @@ import java.util.Locale;
  * </p>
  *
  * @author songyinyin
- * @date 2020/7/18 下午 11:01
- * @since 3.2.0
+ * @since 2020/7/18
  */
-@Component
-public class PowerJobPhysicalNamingStrategy implements PhysicalNamingStrategy, Serializable {
+public class PowerJobPhysicalNamingStrategy extends SpringPhysicalNamingStrategy implements Serializable {
 
-    @Value("${oms.table-prefix:}")
-    private String tablePrefix;
-
-    @Override
-    public Identifier toPhysicalCatalogName(Identifier name, JdbcEnvironment jdbcEnvironment) {
-        return apply(name, jdbcEnvironment);
-    }
-
-    @Override
-    public Identifier toPhysicalSchemaName(Identifier name, JdbcEnvironment jdbcEnvironment) {
-        return apply(name, jdbcEnvironment);
-    }
 
     /**
      * 映射物理表名称，如：把实体表 AppInfoDO 的 DO 去掉，再加上表前缀
@@ -49,65 +33,14 @@ public class PowerJobPhysicalNamingStrategy implements PhysicalNamingStrategy, S
      */
     @Override
     public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment jdbcEnvironment) {
-        Identifier identifier = apply(name, jdbcEnvironment);
 
-        String text = identifier.getText();
+        String tablePrefix = PropertyUtils.getProperties().getProperty("oms.table-prefix");
+
+        String text = name.getText();
         String noDOText = StringUtils.endsWithIgnoreCase(text, "do") ? text.substring(0, text.length() - 2) : text;
         String newText = StringUtils.hasLength(tablePrefix) ? tablePrefix + noDOText : noDOText;
-        return new Identifier(newText, identifier.isQuoted());
+        return super.toPhysicalTableName(new Identifier(newText, name.isQuoted()), jdbcEnvironment);
     }
 
-    @Override
-    public Identifier toPhysicalSequenceName(Identifier name, JdbcEnvironment jdbcEnvironment) {
-        return apply(name, jdbcEnvironment);
-    }
 
-    @Override
-    public Identifier toPhysicalColumnName(Identifier name, JdbcEnvironment jdbcEnvironment) {
-        return apply(name, jdbcEnvironment);
-    }
-
-    private Identifier apply(Identifier name, JdbcEnvironment jdbcEnvironment) {
-        if (name == null) {
-            return null;
-        }
-        StringBuilder builder = new StringBuilder(name.getText().replace('.', '_'));
-        for (int i = 1; i < builder.length() - 1; i++) {
-            if (isUnderscoreRequired(builder.charAt(i - 1), builder.charAt(i), builder.charAt(i + 1))) {
-                builder.insert(i++, '_');
-            }
-        }
-        return getIdentifier(builder.toString(), name.isQuoted(), jdbcEnvironment);
-    }
-
-    /**
-     * Get an identifier for the specified details. By default this method will return an
-     * identifier with the name adapted based on the result of
-     * {@link #isCaseInsensitive(JdbcEnvironment)}
-     *
-     * @param name            the name of the identifier
-     * @param quoted          if the identifier is quoted
-     * @param jdbcEnvironment the JDBC environment
-     * @return an identifier instance
-     */
-    protected Identifier getIdentifier(String name, boolean quoted, JdbcEnvironment jdbcEnvironment) {
-        if (isCaseInsensitive(jdbcEnvironment)) {
-            name = name.toLowerCase(Locale.ROOT);
-        }
-        return new Identifier(name, quoted);
-    }
-
-    /**
-     * Specify whether the database is case sensitive.
-     *
-     * @param jdbcEnvironment the JDBC environment which can be used to determine case
-     * @return true if the database is case insensitive sensitivity
-     */
-    protected boolean isCaseInsensitive(JdbcEnvironment jdbcEnvironment) {
-        return true;
-    }
-
-    private boolean isUnderscoreRequired(char before, char current, char after) {
-        return Character.isLowerCase(before) && Character.isUpperCase(current) && Character.isLowerCase(after);
-    }
 }
