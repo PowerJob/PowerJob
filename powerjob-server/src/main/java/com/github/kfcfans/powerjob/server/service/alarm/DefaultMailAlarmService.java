5c37d6e61e88ee8ec12a12f537871d4e6f17139c
朱八 <jiqi.tjq@alibaba-inc.com>
2020-08-01 23:06:09 +0800

    [dev] optimize JavaMailSender's init

--- powerjob-server/src/main/java/com/github/kfcfans/powerjob/server/service/alarm/DefaultMailAlarmService.java
+++ powerjob-server/src/main/java/com/github/kfcfans/powerjob/server/service/alarm/DefaultMailAlarmService.java
@@ -4,12 +4,14 @@ import com.github.kfcfans.powerjob.common.utils.JsonUtils;
 import com.github.kfcfans.powerjob.server.persistence.core.model.UserInfoDO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -22,19 +24,17 @@ import java.util.List;
 @Service("omsDefaultMailAlarmService")
 public class DefaultMailAlarmService implements Alarmable {
 
-    private final JavaMailSender javaMailSender;
+    @Resource
+    private Environment environment;
 
-    @Value("${spring.mail.username}")
+    private JavaMailSender javaMailSender;
+
     private String from;
+    private static final String FROM_KEY = "spring.mail.username";
 
     private static final String MAIL_TITLE = "PowerJob AlarmService";
     private static final String JOB_INSTANCE_FAILED_CONTENT_PATTERN = "Job run failed, detail is: %s";
     private static final String WF_INSTANCE_FAILED_CONTENT_PATTERN = "Workflow run failed, detail is: %s";
-
-    @Autowired(required = false)
-    public DefaultMailAlarmService(JavaMailSender javaMailSender) {
-        this.javaMailSender = javaMailSender;
-    }
 
     @Override
     public void onJobInstanceFailed(JobInstanceAlarmContent content, List<UserInfoDO> targetUserList) {
@@ -50,9 +50,9 @@ public class DefaultMailAlarmService implements Alarmable {
 
     private void sendMail(String msg, List<UserInfoDO> targetUserList) {
 
+        initFrom();
         log.debug("[OmsMailAlarmService] msg: {}, to: {}", msg, targetUserList);
-
-        if (CollectionUtils.isEmpty(targetUserList) || javaMailSender == null) {
+        if (CollectionUtils.isEmpty(targetUserList) || javaMailSender == null || StringUtils.isEmpty(from)) {
             return;
         }
 
@@ -66,6 +66,18 @@ public class DefaultMailAlarmService implements Alarmable {
             javaMailSender.send(sm);
         }catch (Exception e) {
             log.error("[OmsMailAlarmService] send mail({}) failed, reason is {}", sm, e.getMessage());
+        }
+    }
+
+    @Autowired(required = false)
+    public void setJavaMailSender(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    // 不能直接使用 @Value 注入，不存在的时候会报错
+    private void initFrom() {
+        if (StringUtils.isEmpty(from)) {
+            from = environment.getProperty(FROM_KEY);
         }
     }
 }
