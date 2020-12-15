e311e7c50062115c390c16966c5a8d294eb964b6
tanwenhai <tanwenhai@singsingenglish.com>
2020-12-15 16:11:02 +0800

    id generate

--- powerjob-server/src/main/java/com/github/kfcfans/powerjob/server/persistence/core/repository/ServerInfoRepository.java
+++ powerjob-server/src/main/java/com/github/kfcfans/powerjob/server/persistence/core/repository/ServerInfoRepository.java
@@ -2,6 +2,11 @@ package com.github.kfcfans.powerjob.server.persistence.core.repository;
 
 import com.github.kfcfans.powerjob.server.persistence.core.model.ServerInfoDO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.persistence.LockModeType;
+import java.util.List;
 
 /**
  * 服务器信息 数据操作层
@@ -11,4 +16,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface ServerInfoRepository extends JpaRepository<ServerInfoDO, Long> {
     ServerInfoDO findByIp(String ip);
+
+    @Query("select t from ServerInfoDO as t order by t.id asc")
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    List<ServerInfoDO> findAllAndLockTable();
 }
--- powerjob-server/src/main/java/com/github/kfcfans/powerjob/server/service/id/IdGenerateService.java
+++ powerjob-server/src/main/java/com/github/kfcfans/powerjob/server/service/id/IdGenerateService.java
@@ -3,9 +3,23 @@ package com.github.kfcfans.powerjob.server.service.id;
 import com.github.kfcfans.powerjob.common.utils.NetUtils;
 import com.github.kfcfans.powerjob.server.persistence.core.model.ServerInfoDO;
 import com.github.kfcfans.powerjob.server.persistence.core.repository.ServerInfoRepository;
+import com.google.common.base.Throwables;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.support.JdbcUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.util.Assert;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
 
 /**
  * 唯一ID生成服务，使用 Twitter snowflake 算法
@@ -24,20 +38,32 @@ public class IdGenerateService {
     private static final int DATA_CENTER_ID = 0;
 
     @Autowired
-    public IdGenerateService(ServerInfoRepository serverInfoRepository) {
+    public IdGenerateService(ServerInfoRepository serverInfoRepository,
+                             @Qualifier("coreTransactionManager") PlatformTransactionManager platformTransactionManager) {
+        TransactionTemplate transactionTemplate = new TransactionTemplate(platformTransactionManager);
+        // 服务频繁重启，数据库id自增值过大，使用表锁获取机器id
+        snowFlakeIdGenerator = transactionTemplate.execute(action -> {
+            List<ServerInfoDO> serverInfos = serverInfoRepository.findAllAndLockTable();
+            String ip = NetUtils.getLocalHost();
+            ServerInfoDO server = serverInfoRepository.findByIp(ip);
+            Long id = null;
+            if (server == null) {
+                ServerInfoDO newServerInfo = new ServerInfoDO(ip);
+                serverInfoRepository.saveAndFlush(newServerInfo);
+                id = serverInfos.size() + 1L;
+            } else {
+                for (int i = 0, len = serverInfos.size(); i < len; i++) {
+                    if (Objects.equals(serverInfos.get(i).getId(), server.getId())) {
+                        id = i + 1L;
+                        break;
+                    }
+                }
+            }
+            Assert.notNull(id, "[IdGenerateService] init snowflake error, id is null");
+            log.info("[IdGenerateService] init snowflake for server(address={}) by machineId({}).", ip, id);
 
-        String ip = NetUtils.getLocalHost();
-        ServerInfoDO server = serverInfoRepository.findByIp(ip);
-
-        if (server == null) {
-            ServerInfoDO newServerInfo = new ServerInfoDO(ip);
-            server = serverInfoRepository.saveAndFlush(newServerInfo);
-        }
-
-        Long id = server.getId();
-        snowFlakeIdGenerator = new SnowFlakeIdGenerator(DATA_CENTER_ID, id);
-
-        log.info("[IdGenerateService] init snowflake for server(address={}) by machineId({}).", ip, id);
+            return new SnowFlakeIdGenerator(DATA_CENTER_ID, id);
+        });
     }
 
     /**
