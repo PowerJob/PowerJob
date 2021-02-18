6d9af1cff0660936473cfe726d713bd888dcd4f6
Echo009 <ech0.extreme@foxmail.com>
2021-02-18 12:19:03 +0800

    feat: add WorkflowNodeInfoRepository

---
+++ powerjob-server/src/main/java/com/github/kfcfans/powerjob/server/persistence/core/repository/WorkflowNodeInfoRepository.java
@@ -0,0 +1,18 @@
+package com.github.kfcfans.powerjob.server.persistence.core.repository;
+
+import com.github.kfcfans.powerjob.server.persistence.core.model.WorkflowNodeInfoDO;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+/**
+ * WorkflowNodeInfo 数据访问层
+ *
+ * @author Echo009
+ * @since 2021/2/1
+ */
+public interface WorkflowNodeInfoRepository extends JpaRepository<WorkflowNodeInfoDO, Long> {
+
+
+
+
+}
