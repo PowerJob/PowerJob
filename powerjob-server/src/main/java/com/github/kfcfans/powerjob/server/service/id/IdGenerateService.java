91bb28abbbed9fc10970edd6010c91c78ba63010
tjq <tengjiqi@gmail.com>
2021-02-21 12:26:36 +0800

    feat: adopt a new server-id generation policy to solve the problem of long IDs #220

--- powerjob-server/src/main/java/com/github/kfcfans/powerjob/server/service/id/IdGenerateService.java
+++ powerjob-server/src/main/java/com/github/kfcfans/powerjob/server/service/id/IdGenerateService.java
@@ -1,14 +1,9 @@
 package com.github.kfcfans.powerjob.server.service.id;
 
-import com.github.kfcfans.powerjob.common.PowerJobException;
-import com.github.kfcfans.powerjob.server.extension.ServerIdProvider;
-import com.google.common.collect.Lists;
+import com.github.kfcfans.powerjob.server.remote.server.ServerInfoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
-
-import java.util.List;
 
 /**
  * 唯一ID生成服务，使用 Twitter snowflake 算法
@@ -26,34 +21,11 @@ public class IdGenerateService {
     private static final int DATA_CENTER_ID = 0;
 
     @Autowired
-    public IdGenerateService(List<ServerIdProvider> serverIdProviders) {
+    public IdGenerateService(ServerInfoService serverInfoService) {
 
-        if (CollectionUtils.isEmpty(serverIdProviders)) {
-            throw new PowerJobException("can't find any ServerIdProvider!");
-        }
-
-        ServerIdProvider serverIdProvider;
-        int severIpProviderNums = serverIdProviders.size();
-        if (severIpProviderNums == 1) {
-            serverIdProvider = serverIdProviders.get(0);
-        } else {
-            List<ServerIdProvider> extendServerIpProviders = Lists.newArrayList();
-            for (ServerIdProvider sp : serverIdProviders) {
-                if (sp instanceof DefaultServerIdProvider) {
-                    continue;
-                }
-                extendServerIpProviders.add(sp);
-            }
-            int extNum = extendServerIpProviders.size();
-            if (extNum != 1) {
-                throw new PowerJobException(String.format("find %d ServerIdProvider but just need one, please delete the useless ServerIdProvider!", extNum));
-            }
-            serverIdProvider = extendServerIpProviders.get(0);
-        }
-
-        long id = serverIdProvider.getServerId();
+        long id = serverInfoService.getServerId();
         snowFlakeIdGenerator = new SnowFlakeIdGenerator(DATA_CENTER_ID, id);
-        log.info("[IdGenerateService] initialize IdGenerateService successfully, ServerIdProvider:{},ID:{}", serverIdProvider.getClass().getSimpleName(), id);
+        log.info("[IdGenerateService] initialize IdGenerateService successfully, ID:{}", id);
     }
 
     /**
