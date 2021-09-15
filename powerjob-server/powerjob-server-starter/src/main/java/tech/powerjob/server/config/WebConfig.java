8db9a5b305a3fa0a99516cd3905381224a6d1e4e
欧宝钿 <oubaodian@corp.netease.com>
2021-09-15 09:37:22 +0800

    chore: update auth config

--- powerjob-server/powerjob-server-starter/src/main/java/tech/powerjob/server/config/WebConfig.java
+++ powerjob-server/powerjob-server-starter/src/main/java/tech/powerjob/server/config/WebConfig.java
@@ -45,7 +45,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AuthInterceptor(authService, authConfig))
-                .excludePathPatterns("/auth/**","/openApi/**","/health/**")
+                .excludePathPatterns("/auth/**","/openApi/**","/health/**","/server/**")
                 .addPathPatterns("/**");
     }
 }
