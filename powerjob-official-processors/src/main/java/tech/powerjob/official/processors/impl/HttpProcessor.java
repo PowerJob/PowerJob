ff06c3a961282de690d4a6e751e9aa68a4423a45
Echo009 <ech0.extreme@foxmail.com>
2021-09-28 16:20:43 +0800

    feat: chronos-processor,enhance HttpProcessor

--- powerjob-official-processors/src/main/java/tech/powerjob/official/processors/impl/HttpProcessor.java
+++ powerjob-official-processors/src/main/java/tech/powerjob/official/processors/impl/HttpProcessor.java
@@ -3,6 +3,11 @@ package tech.powerjob.official.processors.impl;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.JSONValidator;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import tech.powerjob.worker.core.processor.ProcessResult;
 import tech.powerjob.worker.core.processor.TaskContext;
 import tech.powerjob.worker.log.OmsLogger;
@@ -30,8 +35,14 @@ public class HttpProcessor extends CommonBasicProcessor {
      * Default timeout is 60 seconds.
      */
     private static final int DEFAULT_TIMEOUT = 60;
+
     private static final int HTTP_SUCCESS_CODE = 200;
+
     private static final Map<Integer, OkHttpClient> CLIENT_STORE = new ConcurrentHashMap<>();
+
+    private static final String HTTP_PREFIX = "http";
+
+    private static final ExpressionParser PARSER = new SpelExpressionParser();
 
     @Override
     public ProcessResult process0(TaskContext taskContext) throws Exception {
@@ -47,8 +58,68 @@ public class HttpProcessor extends CommonBasicProcessor {
         if (StringUtils.isEmpty(httpParams.url)) {
             return new ProcessResult(false, "url can't be empty!");
         }
+        // 构建请求
+        Request request = constructRequest(omsLogger, httpParams);
 
-        if (!httpParams.url.startsWith("http")) {
+        OkHttpClient client = getClient(httpParams.timeout);
+
+        Response response = client.newCall(request).execute();
+        omsLogger.info("response: {}", response);
+
+        String msgBody = "";
+        if (response.body() != null) {
+            msgBody = response.body().string();
+        }
+        int responseCode = response.code();
+        boolean success = true;
+        String res = String.format("code:%d, body:%s", responseCode, msgBody);
+        // 判断结果
+        if (StringUtils.isNotBlank(httpParams.getResultJudgeSpEl())) {
+            success = judgeBySpEl(httpParams, response, msgBody, responseCode, omsLogger);
+            res = res + ", resultJudgeSpEL:" + httpParams.getResultJudgeSpEl();
+        } else {
+            // 默认根据 http 状态码是否等于 200 来判断是否成功
+            if (responseCode != HTTP_SUCCESS_CODE) {
+                success = false;
+                omsLogger.warn("{} url: {} failed, response code is {}, response body is {}",
+                        httpParams.method, httpParams.url, responseCode, msgBody);
+            }
+        }
+        return new ProcessResult(success, res);
+    }
+
+
+    private boolean judgeBySpEl(HttpParams httpParams, Response response, String msgBody, int responseCode, OmsLogger omsLogger) {
+        try {
+            // 构造表达式上下文
+            EvaluationContext context = new StandardEvaluationContext();
+            context.setVariable("responseCode", responseCode);
+            context.setVariable("responseHeaders", response.headers());
+            context.setVariable("responseBodyStr", msgBody);
+            context.setVariable("responseBodyObject", parseIgnoreException(msgBody));
+            Expression expression = PARSER.parseExpression(httpParams.getResultJudgeSpEl());
+            Boolean res = expression.getValue(context, Boolean.class);
+            return res != null && res;
+        } catch (Exception e) {
+            omsLogger.error("fail to parse resultJudgeSpEl, please check it !",e);
+            return false;
+        }
+    }
+
+
+    private JSONObject parseIgnoreException(String json) {
+
+        try {
+            return JSON.parseObject(json);
+        } catch (Exception e) {
+            return new JSONObject();
+        }
+    }
+
+
+    private Request constructRequest(OmsLogger omsLogger, HttpParams httpParams) {
+        if (!httpParams.url.startsWith(HTTP_PREFIX)) {
+            // 默认使用 http 协议
             httpParams.url = "http://" + httpParams.url;
         }
         omsLogger.info("request url: {}", httpParams.url);
@@ -80,8 +151,6 @@ public class HttpProcessor extends CommonBasicProcessor {
             httpParams.timeout = DEFAULT_TIMEOUT;
         }
         omsLogger.info("request timeout: {} seconds", httpParams.timeout);
-        OkHttpClient client = getClient(httpParams.timeout);
-
         Request.Builder builder = new Request.Builder().url(httpParams.url);
         if (httpParams.headers != null) {
             httpParams.headers.forEach((k, v) -> {
@@ -102,24 +171,7 @@ public class HttpProcessor extends CommonBasicProcessor {
             default:
                 builder.get();
         }
-
-        Response response = client.newCall(builder.build()).execute();
-        omsLogger.info("response: {}", response);
-
-        String msgBody = "";
-        if (response.body() != null) {
-            msgBody = response.body().string();
-        }
-
-        int responseCode = response.code();
-        String res = String.format("code:%d, body:%s", responseCode, msgBody);
-        boolean success = true;
-        if (responseCode != HTTP_SUCCESS_CODE) {
-            success = false;
-            omsLogger.warn("{} url: {} failed, response code is {}, response body is {}",
-                    httpParams.method, httpParams.url, responseCode, msgBody);
-        }
-        return new ProcessResult(success, res);
+        return builder.build();
     }
 
     @Data
@@ -149,6 +201,10 @@ public class HttpProcessor extends CommonBasicProcessor {
          * timeout for complete calls
          */
         private Integer timeout;
+        /**
+         * 自定义结果判断逻辑
+         */
+        private String resultJudgeSpEl;
     }
 
     private static OkHttpClient getClient(Integer timeout) {
--- powerjob-official-processors/src/test/java/tech/powerjob/official/processors/impl/HttpProcessorTest.java
+++ powerjob-official-processors/src/test/java/tech/powerjob/official/processors/impl/HttpProcessorTest.java
@@ -1,8 +1,11 @@
 package tech.powerjob.official.processors.impl;
 
 import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Maps;
 import org.junit.jupiter.api.Test;
 import tech.powerjob.official.processors.TestUtils;
+
+import java.util.HashMap;
 
 /**
  * HttpProcessorTest
@@ -10,14 +13,50 @@ import tech.powerjob.official.processors.TestUtils;
  * @author tjq
  * @since 2021/1/31
  */
+@SuppressWarnings("squid:S5976")
 class HttpProcessorTest {
+
+    private static final HttpProcessor PROCESSOR = new HttpProcessor();
+
+    @Test
+    void testResultJudge1() throws Exception {
+        HashMap<String, Object> params = Maps.newHashMap();
+        params.put("opt","check");
+        params.put("uid","example@163.com");
+        HttpProcessor.HttpParams httpParams = new HttpProcessor.HttpParams();
+        httpParams.setUrl("https://www.baidu.com");
+        httpParams.setMediaType("application/json");
+        httpParams.setMethod("POST");
+        httpParams.setResultJudgeSpEl("#responseCode.equals(200)");
+        httpParams.setTimeout(30);
+        httpParams.setBody(JSONObject.toJSONString(params));
+        String paramStr = JSONObject.toJSONString(httpParams);
+        System.out.println(paramStr);
+        System.out.println(PROCESSOR.process(TestUtils.genTaskContext(paramStr)));
+    }
+
+
+
+    @Test
+    void testResultJudge2() throws Exception {
+        HttpProcessor.HttpParams httpParams = new HttpProcessor.HttpParams();
+        httpParams.setUrl("http://mail.163.com/fgw/mailsrv-peacock/account/recommend");
+        httpParams.setMethod("GET");
+        httpParams.setResultJudgeSpEl("#responseBodyObject.get('code').equals(200)");
+        httpParams.setTimeout(30);
+        String paramStr = JSONObject.toJSONString(httpParams);
+        System.out.println(paramStr);
+        System.out.println(PROCESSOR.process(TestUtils.genTaskContext(paramStr)));
+
+    }
+
     
     @Test
     void testDefaultMethod() throws Exception {
         String url = "https://www.baidu.com";
         JSONObject params = new JSONObject();
         params.put("url", url);
-        System.out.println(new HttpProcessor().process(TestUtils.genTaskContext(params.toJSONString())));
+        System.out.println(PROCESSOR.process(TestUtils.genTaskContext(params.toJSONString())));
     }
 
     @Test
@@ -27,7 +66,7 @@ class HttpProcessorTest {
         params.put("url", url);
         params.put("method", "GET");
 
-        System.out.println(new HttpProcessor().process(TestUtils.genTaskContext(params.toJSONString())));
+        System.out.println(PROCESSOR.process(TestUtils.genTaskContext(params.toJSONString())));
     }
 
     @Test
@@ -39,7 +78,7 @@ class HttpProcessorTest {
         params.put("mediaType", "application/json");
         params.put("body", params.toJSONString());
 
-        System.out.println(new HttpProcessor().process(TestUtils.genTaskContext(params.toJSONString())));
+        System.out.println(PROCESSOR.process(TestUtils.genTaskContext(params.toJSONString())));
     }
     
     @Test
@@ -48,7 +87,7 @@ class HttpProcessorTest {
         JSONObject params = new JSONObject();
         params.put("url", url);
         params.put("method", "POST");
-        System.out.println(new HttpProcessor().process(TestUtils.genTaskContext(params.toJSONString())));
+        System.out.println(PROCESSOR.process(TestUtils.genTaskContext(params.toJSONString())));
     }
     
     @Test
@@ -58,7 +97,7 @@ class HttpProcessorTest {
         params.put("url", url);
         params.put("method", "POST");
         params.put("mediaType", "application/json");
-        System.out.println(new HttpProcessor().process(TestUtils.genTaskContext(params.toJSONString())));
+        System.out.println(PROCESSOR.process(TestUtils.genTaskContext(params.toJSONString())));
     }
 
     @Test
@@ -68,6 +107,6 @@ class HttpProcessorTest {
         params.put("url", url);
         params.put("method", "GET");
         params.put("timeout", 20);
-        System.out.println(new HttpProcessor().process(TestUtils.genTaskContext(params.toJSONString())));
+        System.out.println(PROCESSOR.process(TestUtils.genTaskContext(params.toJSONString())));
     }
 }
