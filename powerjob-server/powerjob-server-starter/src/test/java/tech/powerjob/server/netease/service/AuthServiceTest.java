6c6072461cfcc9963bcc71a751da7158e3cb59db
Echo009 <ech0.extreme@foxmail.com>
2022-01-26 14:22:08 +0800

    test: add test

--- powerjob-server/powerjob-server-starter/src/test/java/tech/powerjob/server/netease/service/AuthServiceTest.java
+++ powerjob-server/powerjob-server-starter/src/test/java/tech/powerjob/server/netease/service/AuthServiceTest.java
@@ -1,11 +1,37 @@
 package tech.powerjob.server.netease.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+import tech.powerjob.server.netease.config.AuthConfig;
+import tech.powerjob.server.netease.service.impl.AuthServiceImpl;
+import tech.powerjob.server.persistence.remote.model.UserInfoDO;
+
 
 /**
  * @author Echo009
  * @since 2021/9/7
  */
-class AuthServiceTest {
+@Slf4j
+public class AuthServiceTest {
+
+
+    @Test
+    public void testGenerateJsonWebToken() {
+        AuthConfig authConfig = new AuthConfig();
+        authConfig.setJwtKey("chronos");
+        AuthServiceImpl authService = new AuthServiceImpl(authConfig);
+
+        UserInfoDO userInfoDO = new UserInfoDO();
+        userInfoDO.setId(999999L);
+        userInfoDO.setUsername("ADMIN");
+        userInfoDO.setPassword("*");
+        userInfoDO.setEmail("ADMIN@corp.netease.com");
+        // 生成 本地环境 用的 永久 token
+        String token = authService.generateJsonWebToken(userInfoDO);
+        log.info(token);
+        Assert.assertNotNull(token);
+    }
+
 
 }
