50b4ca3cca448dc9c486568fd0186062fc76279c
tjq <tengjiqi@gmail.com>
2023-01-08 18:30:44 +0800

    feat: optimize resource clean

--- powerjob-remote/powerjob-remote-impl-http/src/main/java/tech/powerjob/remote/http/HttpVertxCSInitializer.java
+++ powerjob-remote/powerjob-remote-impl-http/src/main/java/tech/powerjob/remote/http/HttpVertxCSInitializer.java
@@ -142,8 +142,8 @@ public class HttpVertxCSInitializer implements CSInitializer {
 
     @Override
     public void close() throws IOException {
-        vertx.close();
-        httpServer.close();
         httpClient.close();
+        httpServer.close();
+        vertx.close();
     }
 }
