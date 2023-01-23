e73675ce09aedf30a530e61d164d32610425d47d
tjq <tengjiqi@gmail.com>
2023-01-23 10:18:59 +0800

    feat: add comment for ProcessorFactory

--- powerjob-worker/src/main/java/tech/powerjob/worker/extension/processor/ProcessorFactory.java
+++ powerjob-worker/src/main/java/tech/powerjob/worker/extension/processor/ProcessorFactory.java
@@ -1,10 +1,13 @@
 package tech.powerjob.worker.extension.processor;
 
+import tech.powerjob.common.enums.ProcessorType;
+
 import java.util.Set;
 
 /**
  * 处理器工厂
  * 考虑到当前是一个百花齐放的生态，各种 IOC 框架层出不穷。PowerJob 决定在 4.3.0 剥离对 Spring 的强依赖，并允许开发者自定义 Bean 的初始化逻辑
+ * 不知道怎么用的话，可以看看官方提供的 3 个默认实现，比如对接第三方 IOC 框架就类似于 BuiltInSpringProcessorFactory
  *
  * @author tjq
  * @since 2023/1/17
@@ -13,6 +16,7 @@ public interface ProcessorFactory {
 
     /**
      * 支持的处理器类型，类型不匹配则跳过该 ProcessorFactory 的加载逻辑
+     * 对应的是控制台的'处理器类型' TAB，不做任何定制的情况下，取值范围为 {@link ProcessorType#name()}
      * @return 支持的处理器类型
      */
     Set<String> supportTypes();
