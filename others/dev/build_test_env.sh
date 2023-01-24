6921cfdcf5ba1d00f1730414f8779e1cc86e5cdb
tjq <tengjiqi@gmail.com>
2023-01-24 16:10:53 +0800

    chore: optimize test env

--- others/dev/build_test_env.sh
+++ others/dev/build_test_env.sh
@@ -7,7 +7,7 @@ echo "================== 构建 jar =================="
 cd `dirname $0`/../.. || exit
 # mvn clean package -Pdev -DskipTests -U -e -pl powerjob-server,powerjob-worker-agent -am
 # -U：强制检查snapshot库 -pl：指定需要构建的模块，多模块逗号分割 -am：同时构建依赖模块，一般与pl连用 -Pxxx：指定使用的配置文件
-mvn clean package -Pdev -DskipTests -pl powerjob-server,powerjob-worker-agent -am
+mvn clean package -Pdev -DskipTests -pl powerjob-server-starter,powerjob-worker-agent -am
 echo "================== 拷贝 jar =================="
 /bin/cp -rf powerjob-server/powerjob-server-starter/target/*.jar powerjob-server/docker/powerjob-server.jar
 /bin/cp -rf powerjob-worker-agent/target/*.jar powerjob-worker-agent/powerjob-agent.jar
