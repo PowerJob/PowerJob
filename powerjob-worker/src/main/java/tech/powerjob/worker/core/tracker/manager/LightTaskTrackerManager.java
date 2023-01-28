5d9ce2daf0d9e2949d73058e24d3777db014379c
tjq <tengjiqi@gmail.com>
2023-01-28 10:13:28 +0800

    fix: bug of deadlock

--- powerjob-worker/src/main/java/tech/powerjob/worker/core/tracker/manager/LightTaskTrackerManager.java
+++ powerjob-worker/src/main/java/tech/powerjob/worker/core/tracker/manager/LightTaskTrackerManager.java
@@ -21,8 +21,11 @@ public class LightTaskTrackerManager {
         return INSTANCE_ID_2_TASK_TRACKER.get(instanceId);
     }
 
-    public static LightTaskTracker removeTaskTracker(Long instanceId) {
-        return INSTANCE_ID_2_TASK_TRACKER.remove(instanceId);
+    public static void removeTaskTracker(Long instanceId) {
+        // 忽略印度的 IDE 警告，这个判断非常有用！！！不加这个判断会导致：如果创建 TT（先执行 computeIfAbsent 正在将TT添加到 HashMap） 时报错，TT 主动调用 destroy 销毁（从 HashMap移除该 TT）时死锁
+        if (INSTANCE_ID_2_TASK_TRACKER.containsKey(instanceId)) {
+            INSTANCE_ID_2_TASK_TRACKER.remove(instanceId);
+        }
     }
 
     public static void atomicCreateTaskTracker(Long instanceId, Function<Long, LightTaskTracker> creator) {
