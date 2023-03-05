3fcd99e3640a91fc30a0f7a1336f256a0b0041d4
tjq <tengjiqi@gmail.com>
2023-03-05 15:43:55 +0800

    feat: optimize pr code

--- powerjob-server/powerjob-server-starter/src/main/java/tech/powerjob/server/web/controller/JobController.java
+++ powerjob-server/powerjob-server-starter/src/main/java/tech/powerjob/server/web/controller/JobController.java
@@ -88,22 +88,26 @@ public class JobController {
         if (request.getJobId() != null) {
 
             Optional<JobInfoDO> jobInfoOpt = jobInfoRepository.findById(request.getJobId());
-            if (!jobInfoOpt.get().getAppId().equals(request.getAppId())){
-                return ResultDTO.failed("请输入该app下的jobId");
-            }
-            PageResult<JobInfoVO> result = new PageResult<>();
-            result.setIndex(0);
-            result.setPageSize(request.getPageSize());
 
-            if (jobInfoOpt.isPresent()) {
-                result.setTotalItems(1);
-                result.setTotalPages(1);
-                result.setData(Lists.newArrayList(JobInfoVO.from(jobInfoOpt.get())));
-            } else {
+            PageResult<JobInfoVO> result = new PageResult<>();
+
+            if (!jobInfoOpt.isPresent()) {
                 result.setTotalPages(0);
                 result.setTotalItems(0);
                 result.setData(Lists.newLinkedList());
+                return ResultDTO.success(result);
             }
+
+            if (!jobInfoOpt.get().getAppId().equals(request.getAppId())){
+                return ResultDTO.failed("请输入该app下的jobId");
+            }
+
+            result.setIndex(0);
+            result.setPageSize(request.getPageSize());
+
+            result.setTotalItems(1);
+            result.setTotalPages(1);
+            result.setData(Lists.newArrayList(JobInfoVO.from(jobInfoOpt.get())));
 
             return ResultDTO.success(result);
         }
