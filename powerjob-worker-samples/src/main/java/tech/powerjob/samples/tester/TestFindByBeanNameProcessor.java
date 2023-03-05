81752dd26f3f28ab71da95da4a1a0616b8d65ee7
tjq <tengjiqi@gmail.com>
2023-03-05 15:58:54 +0800

    test: TestFindByBeanNameProcessor

---
+++ powerjob-worker-samples/src/main/java/tech/powerjob/samples/tester/TestFindByBeanNameProcessor.java
@@ -0,0 +1,21 @@
+package tech.powerjob.samples.tester;
+
+import org.springframework.stereotype.Component;
+import tech.powerjob.worker.core.processor.ProcessResult;
+import tech.powerjob.worker.core.processor.TaskContext;
+import tech.powerjob.worker.core.processor.sdk.BasicProcessor;
+
+/**
+ * 测试直接使用 BeanName 获取处理器
+ *
+ * @author tjq
+ * @since 2023/3/5
+ */
+@Component(value = "powerJobTestBeanNameProcessor")
+public class TestFindByBeanNameProcessor implements BasicProcessor {
+    @Override
+    public ProcessResult process(TaskContext context) throws Exception {
+        System.out.println("======== IN =======");
+        return new ProcessResult(true, "Welcome to use PowerJob~");
+    }
+}
