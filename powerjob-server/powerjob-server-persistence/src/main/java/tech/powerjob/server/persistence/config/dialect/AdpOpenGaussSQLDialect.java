b9d47f1bd3a8f599a12feafe120640576e211bab
chenshouye <jupiterskiss@163.com>
2024-11-12 15:50:55 +0800

    Add a new  AdpOpenGaussSQLDialect to deal with OpenGaussDialect

---
+++ powerjob-server/powerjob-server-persistence/src/main/java/tech/powerjob/server/persistence/config/dialect/AdpOpenGaussSQLDialect.java
@@ -0,0 +1,37 @@
+package tech.powerjob.server.persistence.config.dialect;
+
+import org.hibernate.dialect.PostgreSQL10Dialect;
+import org.hibernate.type.descriptor.sql.LongVarbinaryTypeDescriptor;
+import org.hibernate.type.descriptor.sql.LongVarcharTypeDescriptor;
+import org.hibernate.type.descriptor.sql.SqlTypeDescriptor;
+
+import java.sql.Types;
+
+/**
+ * 拷贝自AdpPostgreSQLDialect，用来处理OpenGauss相关数据类型发言
+ * 使用方自行通过配置文件激活：spring.datasource.remote.hibernate.properties.hibernate.dialect=tech.powerjob.server.persistence.config.dialect.AdpOpenGaussSQLDialect
+ *
+ * @author litong0531
+ * @since 2024/8/11
+ */
+public class AdpOpenGaussSQLDialect extends PostgreSQL10Dialect {
+
+    public AdpOpenGaussSQLDialect() {
+        super();
+        registerColumnType(Types.BLOB, "bytea");
+        registerColumnType(Types.CLOB, "text");
+    }
+
+    @Override
+    public SqlTypeDescriptor remapSqlTypeDescriptor(SqlTypeDescriptor sqlTypeDescriptor) {
+        switch (sqlTypeDescriptor.getSqlType()) {
+            case Types.CLOB:
+                return LongVarcharTypeDescriptor.INSTANCE;
+            case Types.BLOB:
+                return LongVarbinaryTypeDescriptor.INSTANCE;
+            case Types.NCLOB:
+                return LongVarbinaryTypeDescriptor.INSTANCE;
+        }
+        return super.remapSqlTypeDescriptor(sqlTypeDescriptor);
+    }
+}
